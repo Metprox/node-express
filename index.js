ae3ae857be253b51abd93d2e717c934c9b9fd324
Oleg Vakarchuk <oleg@digitalmaker.io>
2020-01-27 17:53:28 +0200

    routers and more

--- index.js
+++ index.js
@@ -1,6 +1,31 @@
 const express = require("express");
+const path = require("path");
+const exphbs = require("express-handlebars");
+const homeRoutes = require("./routes/home");
+const addRoutes = require("./routes/add");
+const coursesRoutes = require("./routes/courses");
+
 const app = express();
 
+const hbs = exphbs.create({
+  defaultLayout: "main",
+  extname: "hbs",
+});
+
+// setting up
+app.engine("hbs", hbs.engine);
+app.set("view engine", "hbs");
+app.set("views", "views");
+
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+
+// Routers
+app.use("/", homeRoutes);
+app.use("/add", addRoutes);
+app.use("/courses", coursesRoutes);
+
+// Port
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
