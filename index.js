613a2a46fef0818598e9d8c28e249d0ece8ced1c
metprox <olegvakarchuk94@gmail.com>
2020-01-29 23:29:33 +0200

    refactor all project for correct job

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const path = require("path");
+const mongoose = require("mongoose");
 const exphbs = require("express-handlebars");
 const homeRoutes = require("./routes/home");
 const cardRoutes = require("./routes/card");
 const addRoutes = require("./routes/add");
 const coursesRoutes = require("./routes/courses");
+const User = require("./models/user");
 
 const app = express();
 
@@ -13,23 +15,55 @@ const hbs = exphbs.create({
   extname: "hbs"
 });
 
-// setting up
 app.engine("hbs", hbs.engine);
 app.set("view engine", "hbs");
 app.set("views", "views");
 
+app.use(async (req, res, next) => {
+  try {
+    const user = await User.findById("5e31f473ecd47e349c5f520e");
+    req.user = user;
+    next();
+  } catch (err) {
+    console.log(err);
+  }
+});
+
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
 
-// Routers
 app.use("/", homeRoutes);
 app.use("/add", addRoutes);
 app.use("/courses", coursesRoutes);
 app.use("/card", cardRoutes);
 
-// Port
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+async function start() {
+  try {
+    const url = `mongodb+srv://oleg:so7YEVXSvpbkDqEy@cluster0-kucyo.mongodb.net/shop?w=majority`;
+    await mongoose.connect(url, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false
+    });
+    const candidate = await User.findOne();
+    if (!candidate) {
+      const user = new User({
+        email: "oleg@gmail.com",
+        name: "Oleg",
+        cart: { items: [] }
+      });
+
+      await user.save();
+    }
+
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+start();
